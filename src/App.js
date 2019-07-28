153cddbdaabee91aa1cf371570d933f091c6b8c0
James Gary <gmarchesi888@gmail.com>
2019-07-28 09:00:08 -0400

    Getting started.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./Haaser_v1.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidenav">
+        <img src={logo} className="logo" alt="logo" />
+        <a href="#">Home</a>
+        <a href="#">Downloads</a>
+        <a href="#">FB</a>
+        <a href="#">Twitter</a>
+        <a href="#">Instagram</a>
+      </div>
+
+      <div className="main">
+        <h1>Main page</h1>
+      </div>
     </div>
   );
 }
