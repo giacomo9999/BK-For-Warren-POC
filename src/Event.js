709ec9c29a73fa5acc61de7b2f02cf7fee58ddc5
James Gary <gmarchesi888@gmail.com>
2019-07-30 08:16:05 -0400

    Added functionality to Event buttons.

--- src/Event.js
+++ src/Event.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Event = props => {
-  return (
-    <div className="container-inner">
-      <h2>{`${props.eventDate}  • ${props.eventName}`}</h2>
-      <p>{props.eventDescription}</p>
-      <div className="spacer10" />
-      <button>Sign Up!</button>
-      <div className="spacer10" />
-    </div>
-  );
-};
+class Event extends Component {
+  state = { userGoing: false };
+  toggleUserGoing = () => {
+    console.log("User going? " + this.state.userGoing);
+    this.setState({ userGoing: !this.state.userGoing });
+  };
+  render() {
+    console.log("RM User going? " + this.state.userGoing);
+    return (
+      <div className="container-inner">
+        <h2>{`${this.props.eventDate}  • ${this.props.eventName}`}</h2>
+        <p>{this.props.eventDescription}</p>
+        <div className="spacer10" />
+        <div>
+          {this.state.userGoing ? (
+            <div>
+              <button>You're Going!</button>
+              <button onClick={this.toggleUserGoing}>Cancel</button>
+              <button>Add To Google Calendar</button>
+            </div>
+          ) : (
+            <button onClick={this.toggleUserGoing}>Sign Up</button>
+          )}
+        </div>
+        <div className="spacer10" />
+      </div>
+    );
+  }
+}
 
 export default Event;
